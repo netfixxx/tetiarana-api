501a6ab630270fa793a5b3a6de54a3e40ed6f4d2
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-17 21:48:55 +0300

    chore: add cors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const todoRoutes = require('./routes/todoRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Utiliser les routes pour les todos
 app.use('/todos', todoRoutes);
 app.use('/users', userRoutes);
 
-// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
